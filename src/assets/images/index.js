68fd8c1274925588e3082553273658c7362a6d20
alicendeh <alicendeh16@gmail.com>
2021-12-23 17:54:01 +0100

    Done with the navbar

---
+++ src/assets/images/index.js
@@ -0,0 +1,31 @@
+import bg_boost_desktop from "./bg-boost-desktop.svg";
+import bg_boost_mobile from "./bg-boost-mobile.svg";
+import bg_shorten_desktop from "./bg-shorten-desktop.svg";
+import bg_shorten_mobile from "./bg-shorten-mobile.svg";
+import favicon from "./favicon-32x32.png";
+import icon_brand_recognition from "./icon-brand-recognition.svg";
+import icon_detailed_records from "./icon-detailed-records.svg";
+import icon_facebook from "./icon-facebook.svg";
+import icon_fully_customizable from "./icon-fully-customizable.svg";
+import icon_instagram from "./icon-instagram.svg";
+import icon_pinterest from "./icon-pinterest.svg";
+import icon_twitter from "./icon-twitter.svg";
+import illustration_working from "./illustration-working.svg";
+import logo from "./logo.svg";
+
+export {
+  bg_boost_desktop,
+  bg_boost_mobile,
+  bg_shorten_desktop,
+  bg_shorten_mobile,
+  favicon,
+  icon_brand_recognition,
+  icon_twitter,
+  icon_pinterest,
+  icon_facebook,
+  icon_detailed_records,
+  icon_fully_customizable,
+  icon_instagram,
+  illustration_working,
+  logo,
+};
--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { logo } from "../../assets/images";
+import "./Header.style.css";
 
 function Header() {
+  const [menuToggler, setmenuToggler] = useState(false);
+
+  const toggleMenu = () => {
+    setmenuToggler(!menuToggler);
+  };
   return (
-    <div>
-      <div>
-        <p>Logo</p>
-        <p>Features</p>
-        <p>Pricing</p>
-        <p>Resources</p>
+    <>
+      <nav className="navWrapper">
+        <div className="firstSection">
+          <img src={logo} alt="logo" />
+          <p>Features</p>
+          <p>Pricing</p>
+          <p>Resources</p>
+        </div>
+        <div className="secondSection">
+          <p>Login</p>
+          <button>Signup</button>
+        </div>
+        <div className="menu" onClick={toggleMenu}>
+          {menuToggler ? (
+            <i class="fas fa-times"></i>
+          ) : (
+            <i class="fas fa-bars"></i>
+          )}
+        </div>
+      </nav>
+      <div
+        className={`hey ${menuToggler ? "hamburgerMenu" : "show"} `}
+        // "hamburgerMenu"
+      >
+        <div>
+          <p>Features</p>
+          <p>Pricing</p>
+          <p>Resources</p>
+        </div>
+        <div className="line">
+          <hr />
+        </div>
+        <div>
+          <p>Login</p>
+          <button>Signup</button>
+        </div>
       </div>
-      <div></div>
-    </div>
+    </>
   );
 }
 
