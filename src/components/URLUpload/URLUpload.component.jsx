fd299c641745c57b3cabe5e2c6b7194a1a51995b
alicendeh <alicendeh16@gmail.com>
2022-01-09 12:35:59 +0100

    DOne with setting the url to storage

--- src/components/URLUpload/URLUpload.component.jsx
+++ src/components/URLUpload/URLUpload.component.jsx
@@ -1,29 +1,100 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { bg_shorten_desktop } from "../../assets/images";
-import { Button } from "../../components";
 import "./URLUpload.style.css";
 
 function URLUpload() {
+  const [link, setlink] = useState("");
+  const [isEmpty, setisEmpty] = useState(false);
+  const [shortenedLink, setshortenedLink] = useState([]);
+
+  useEffect(() => {
+    let p = localStorage.getItem("LINK");
+    console.log(p, "p here");
+  }, []);
+
+  useEffect(() => {
+    let exists = localStorage.getItem("EXISTS");
+    let links = JSON.parse(localStorage.getItem("LINKS"));
+
+    if (exists === null) {
+      setshortenedLink([]);
+    } else {
+      setshortenedLink([...links]);
+    }
+  }, []);
+
+  const shortenLink = async () => {
+    if (link === "") {
+      setisEmpty(true);
+    } else {
+      let res = await axios.get(`
+        https://api.shrtco.de/v2/shorten?url=${link}/very/long/link.html
+        `);
+      await setshortenedLink([
+        ...shortenedLink,
+        {
+          fullLink: link,
+          shortenedLink: res.data.result.short_link,
+        },
+      ]);
+
+      localStorage.setItem(
+        "LINKS",
+        JSON.stringify([
+          ...shortenedLink,
+          {
+            fullLink: link,
+            shortenedLink: res.data.result.short_link,
+          },
+        ])
+      );
+      if (res.data) {
+        localStorage.setItem("EXISTS", true);
+      }
+    }
+  };
+
+  const onChange = (e) => {
+    setlink(e.target.value);
+  };
+
   return (
     <div className="mainDiv">
       <div className="imgBackground">
         <div className="mainContainer">
           <img src={bg_shorten_desktop} alt="" className="imgView" />
           <div className="inputContainer">
-            <input placeholder="shorten a link here..." className="input" />
-            {/* <Button
-              mys={{
-                width: "70%",
-              }}
-              className="extra"
-              title="Shorten It!"
-              br="7px"
-              width="150px"
-            /> */}
-            <button className="myBtn">Shorten It!</button>
+            <input
+              onChange={onChange}
+              value={link}
+              placeholder="shorten a link here..."
+              className={isEmpty ? `placeholdered` : "input"}
+            />
+            {isEmpty && <i className="noLink">Please add a link</i>}
+
+            <button className="myBtn" onClick={shortenLink}>
+              Shorten It!
+            </button>
           </div>
         </div>
       </div>
+      {shortenedLink.length > 0 && (
+        <div className="shotenLinkContainer">
+          {shortenedLink.map((data, index) => (
+            <div className="whiteContainer" key={index}>
+              <div>
+                <p className="full">{data.fullLink}</p>
+              </div>
+              <div className="myLine"></div>
+              <div className="secondContent">
+                <p>{data.shortenedLink} </p>
+                <button className={`myBtn enlarge`}>Copy</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
